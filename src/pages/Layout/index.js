c7ddab5774fd61b9c88e634378be6040b64e4447
lcy <34083314@qq.com>
2025-09-23 21:47:03 +0800

    feat(react-bill): 完成主题颜色定制化

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
-
+import { Button } from "antd-mobile";
 function Layout() {
   return (
     <>
       <div>
         this is Layout
       </div>
+      <Button color="primary">
+        测试按钮
+      </Button>
       <Outlet />
     </>
   )
