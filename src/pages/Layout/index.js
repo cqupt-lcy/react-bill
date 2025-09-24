62e8c0c1a24c98a57c2f355cf61579b7e394c167
lcy <34083314@qq.com>
2025-09-24 14:27:15 +0800

    tarbar切换功能

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,26 +1,54 @@
-import { Outlet } from "react-router-dom";
-import { Button } from "antd-mobile";
+import { Outlet, useNavigate } from "react-router-dom";
 import { getBillList } from "@/store/modules/billStore";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { TabBar } from "antd-mobile";
+import { BillOutline, CalculatorOutline, AddCircleOutline } from 'antd-mobile-icons'
+import './index.scss'
 function Layout() {
-  const { billList } = useSelector(state => state.bill);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBillList());
   }, [dispatch]);
+  const navigate = useNavigate();
+  const switchRoute = (path) => {
+    navigate(path);  
+  }
+  const tabs = [
+    {
+      key: '/',
+      title: '月度账单',
+      icon: <BillOutline />
+    },
+    {
+      key: '/new',
+      title: '记账',
+      icon: <AddCircleOutline />,
+    },
+    {
+      key: '/year',
+      title: '年度账单',
+      icon: <CalculatorOutline />
+    },
 
+  ]
   return (
-    <>
-      <div>
-        this is Layout
+    <div className="layout">
+      <div className="container">
+        <Outlet />
       </div>
-      <Button color="primary">
-        测试按钮
-      </Button>
-      {billList.map(item => <div>{item.money}</div>)}
-      <Outlet />
-    </>
+      <div className="footer">
+        <TabBar onChange={switchRoute}>
+          {tabs.map(item => (
+            <TabBar.Item
+              key={item.key}
+              icon={item.icon}
+              title={item.title}
+            />
+          ))}
+        </TabBar>
+      </div>
+    </div>
   )
 }
 export default Layout;  
